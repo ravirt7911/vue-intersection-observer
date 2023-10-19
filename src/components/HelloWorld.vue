e351a2ef8307538c19acb91515508a9f290d139e
ravirt7911 <ravirt7911@gmail.com>
2023-10-19 19:27:11 +0530

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,232 @@
+<template>
+	<div class="flex relative">
+		<!-- Text -->
+		<div class="w-3/4 p-4">
+			<div v-for="(heading, index) in headings" :key="index">
+				<h2 class="header text-4xl font-bold" :id="index.toString()">
+					{{ heading }}
+				</h2>
+				<p class="text-gray-600">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+					do eiusmod tempor incididunt ut labore et dolore magna
+					aliqua. Non pulvinar neque laoreet suspendisse interdum
+					consectetur libero id. Vel facilisis volutpat est velit
+					egestas dui id. Elementum sagittis vitae et leo duis ut diam
+					quam. Vitae justo eget magna fermentum. Egestas quis ipsum
+					suspendisse ultrices gravida. Aenean sed adipiscing diam
+					donec adipiscing tristique risus. Enim eu turpis egestas
+					pretium. In eu mi bibendum neque egestas congue quisque
+					egestas. Eu ultrices vitae auctor eu augue ut lectus arcu
+					bibendum. Sed blandit libero volutpat sed. In aliquam sem
+					fringilla ut morbi tincidunt. Nibh sed pulvinar proin
+					gravida hendrerit lectus a. Augue mauris augue neque gravida
+					in. Diam vel quam elementum pulvinar etiam non quam lacus.
+					Faucibus et molestie ac feugiat sed. Ullamcorper sit amet
+					risus nullam eget felis eget nunc lobortis. Aliquet sagittis
+					id consectetur purus. Sagittis eu volutpat odio facilisis
+					mauris sit amet massa. In hac habitasse platea dictumst
+					quisque sagittis purus sit amet. Vulputate enim nulla
+					aliquet porttitor lacus luctus. Nullam eget felis eget nunc
+					lobortis mattis aliquam. Vulputate enim nulla aliquet
+					porttitor lacus luctus accumsan tortor posuere. Eget mi
+					proin sed libero enim sed faucibus turpis in. Aliquet eget
+					sit amet tellus cras adipiscing enim eu turpis. Odio ut sem
+					nulla pharetra diam sit. Habitant morbi tristique senectus
+					et netus et malesuada fames. Justo laoreet sit amet cursus
+					sit amet dictum sit. Porttitor eget dolor morbi non arcu
+					risus quis. Eu augue ut lectus arcu bibendum at varius. At
+					elementum eu facilisis sed odio. Sed euismod nisi porta
+					lorem mollis aliquam ut. Aliquam sem fringilla ut morbi
+					tincidunt augue interdum velit. A arcu cursus vitae congue
+					mauris rhoncus. Risus viverra adipiscing at in tellus
+					integer. Sollicitudin ac orci phasellus egestas tellus
+					rutrum tellus pellentesque. Sagittis nisl rhoncus mattis
+					rhoncus urna neque. Et ultrices neque ornare aenean euismod
+					elementum. Aliquet nibh praesent tristique magna. Porta non
+					pulvinar neque laoreet suspendisse interdum consectetur
+					libero id. Ac turpis egestas integer eget aliquet nibh.
+					Malesuada fames ac turpis egestas integer eget aliquet nibh
+					praesent. Tellus orci ac auctor augue mauris augue neque
+					gravida in. Egestas maecenas pharetra convallis posuere.
+					Ullamcorper eget nulla facilisi etiam dignissim diam quis
+					enim. Facilisis volutpat est velit egestas dui id ornare
+					arcu odio. Pulvinar sapien et ligula ullamcorper malesuada
+					proin. Tristique senectus et netus et. Aenean vel elit
+					scelerisque mauris pellentesque. Mi tempus imperdiet nulla
+					malesuada pellentesque elit eget. Turpis egestas sed tempus
+					urna et pharetra pharetra. Sit amet purus gravida quis
+					blandit turpis cursus. Amet justo donec enim diam vulputate
+					ut. Sit amet consectetur adipiscing elit. Diam phasellus
+					vestibulum lorem sed risus ultricies tristique nulla. Sit
+					amet nisl purus in mollis nunc sed id semper. Consequat
+					mauris nunc congue nisi vitae suscipit tellus. Imperdiet
+					nulla malesuada pellentesque elit eget gravida cum sociis
+					natoque. Feugiat pretium nibh ipsum consequat nisl vel
+					pretium lectus quam. Proin libero nunc consequat interdum.
+					Volutpat diam ut venenatis tellus. Elementum pulvinar etiam
+					non quam. Odio pellentesque diam volutpat commodo sed
+					egestas egestas. Tristique et egestas quis ipsum suspendisse
+					ultrices gravida. Et malesuada fames ac turpis egestas.
+					Euismod quis viverra nibh cras pulvinar mattis. Nunc id
+					cursus metus aliquam. Leo duis ut diam quam. Tristique magna
+					sit amet purus gravida quis blandit turpis. Libero justo
+					laoreet sit amet cursus sit amet dictum. Commodo nulla
+					facilisi nullam vehicula ipsum a arcu cursus vitae. In
+					fermentum posuere urna nec. Vel risus commodo viverra
+					maecenas accumsan. Viverra accumsan in nisl nisi
+					scelerisque. Aliquam id diam maecenas ultricies mi eget
+					mauris. Platea dictumst quisque sagittis purus sit amet
+					volutpat consequat. Nisi vitae suscipit tellus mauris a diam
+					maecenas sed enim. Natoque penatibus et magnis dis
+					parturient montes. Amet consectetur adipiscing elit
+					pellentesque habitant. A pellentesque sit amet porttitor
+					eget dolor morbi non arcu. Pellentesque diam volutpat
+					commodo sed egestas. Dolor morbi non arcu risus quis varius
+					quam quisque. Nec tincidunt praesent semper feugiat nibh sed
+					pulvinar proin. Diam in arcu cursus euismod quis viverra
+					nibh cras. Gravida rutrum quisque non tellus orci ac auctor
+					augue. Viverra justo nec ultrices dui sapien eget mi proin
+					sed. Aliquet eget sit amet tellus cras adipiscing enim.
+					Sodales neque sodales ut etiam sit. Ipsum dolor sit amet
+					consectetur adipiscing elit duis tristique. Morbi tempus
+					iaculis urna id volutpat lacus laoreet non. Sit amet luctus
+					venenatis lectus magna fringilla urna porttitor rhoncus.
+					Malesuada proin libero nunc consequat interdum varius sit
+					amet mattis. Sit amet facilisis magna etiam tempor orci eu.
+					Scelerisque mauris pellentesque pulvinar pellentesque
+					habitant morbi. Condimentum mattis pellentesque id nibh
+					tortor id aliquet. Facilisis gravida neque convallis a cras
+					semper. Mollis nunc sed id semper risus in hendrerit
+					gravida. Arcu dui vivamus arcu felis bibendum ut tristique
+					et egestas. Condimentum mattis pellentesque id nibh tortor
+					id aliquet lectus proin. Venenatis a condimentum vitae
+					sapien pellentesque habitant morbi tristique senectus. Ut
+					pharetra sit amet aliquam id diam maecenas ultricies. Orci
+					porta non pulvinar neque laoreet. Nisl purus in mollis nunc
+					sed id semper risus. Aenean vel elit scelerisque mauris
+					pellentesque pulvinar. Gravida rutrum quisque non tellus.
+					Erat imperdiet sed euismod nisi porta. Ut tristique et
+					egestas quis ipsum suspendisse. Et ultrices neque ornare
+					aenean euismod elementum nisi quis. Feugiat sed lectus
+					vestibulum mattis ullamcorper velit sed ullamcorper. Mauris
+					nunc congue nisi vitae suscipit. Integer enim neque volutpat
+					ac tincidunt vitae semper. Tincidunt vitae semper quis
+					lectus nulla at volutpat. Diam maecenas ultricies mi eget
+					mauris pharetra et. Dui sapien eget mi proin. Id aliquet
+					risus feugiat in ante metus dictum at tempor. Eget nulla
+					facilisi etiam dignissim. Hendrerit dolor magna eget est
+					lorem ipsum dolor sit amet. Interdum posuere lorem ipsum
+					dolor sit. Sit amet luctus venenatis lectus magna fringilla
+					urna. Sed ullamcorper morbi tincidunt ornare massa. Nam at
+					lectus urna duis convallis. Diam sit amet nisl suscipit
+					adipiscing bibendum est ultricies. Accumsan sit amet nulla
+					facilisi morbi tempus iaculis urna. Id neque aliquam
+					vestibulum morbi blandit cursus risus at ultrices. Blandit
+					aliquam etiam erat velit scelerisque in. Quam pellentesque
+					nec nam aliquam sem et. Bibendum at varius vel pharetra vel
+					turpis nunc eget. Adipiscing vitae proin sagittis nisl
+					rhoncus mattis. Mattis vulputate enim nulla aliquet
+					porttitor lacus luctus. Turpis in eu mi bibendum neque.
+					Euismod in pellentesque massa placerat duis. Neque convallis
+					a cras semper auctor. Amet commodo nulla facilisi nullam.
+					Adipiscing elit pellentesque habitant morbi tristique
+					senectus et netus. Sem nulla pharetra diam sit amet nisl
+					suscipit adipiscing bibendum. Sit amet aliquam id diam
+					maecenas ultricies mi eget. Vulputate eu scelerisque felis
+					imperdiet. Aliquet bibendum enim facilisis gravida neque
+					convallis a cras. Quam nulla porttitor massa id. Elit ut
+					aliquam purus sit amet. Faucibus scelerisque eleifend donec
+					pretium vulputate sapien nec sagittis. Maecenas ultricies mi
+					eget mauris pharetra. Ornare lectus sit amet est placerat in
+					egestas. Sit amet commodo nulla facilisi. Vel risus commodo
+					viverra maecenas. Dolor sit amet consectetur adipiscing elit
+					pellentesque. Ultricies tristique nulla aliquet enim tortor
+					at. Maecenas ultricies mi eget mauris pharetra et ultrices
+					neque. Nulla porttitor massa id neque aliquam vestibulum
+					morbi blandit. Lorem ipsum dolor sit amet consectetur
+					adipiscing. Cursus euismod quis viverra nibh cras pulvinar
+					mattis nunc sed. At imperdiet dui accumsan sit amet nulla
+					facilisi morbi. Eget magna fermentum iaculis eu non.
+					Dignissim cras tincidunt lobortis feugiat vivamus at augue
+					eget. Massa tincidunt dui ut ornare. Vivamus at augue eget
+					arcu. Pretium quam vulputate dignissim suspendisse in est
+					ante. Eget nulla facilisi etiam dignissim diam quis enim.
+					ultrices dui sapien eget mi. Ultricies mi eget mauris
+					pharetra et ultrices. Viverra accumsan in nisl nisi
+					scelerisque eu ultrices vitae auctor. Cursus euismod quis
+					viverra nibh cras. Hendrerit dolor magna eget est lorem
+					ipsum. Elit duis tristique sollicitudin nibh. Tellus cras
+					adipiscing enim eu. Volutpat ac tincidunt vitae semper quis
+					lectus nulla at. Sit amet volutpat consequat mauris nunc
+					congue. Integer malesuada nunc vel risus commodo viverra
+					maecenas accumsan lacus. Amet nulla facilisi morbi tempus
+					iaculis. Bibendum arcu vitae elementum curabitur vitae nunc
+					sed velit dignissim. Elementum pulvinar etiam non quam lacus
+					suspendisse faucibus interdum. In egestas erat imperdiet sed
+					euismod. Sapien pellentesque habitant morbi tristique
+					senectus. Tristique risus nec feugiat in fermentum posuere
+					urna nec tincidunt. Eu facilisis sed odio morbi quis commodo
+					odio aenean. Nulla facilisi nullam vehicula ipsum a arcu.
+				</p>
+			</div>
+		</div>
+
+		<!-- Picture -->
+		<div class="w-1/4 p-4">
+  <div class="fixed-picture-container">
+    <div
+      v-for="(heading, index) in headings"
+      :key="index"
+      class="cursor-pointer transition-colors hover:bg-gray-100 w-56"
+    >
+      <img
+        v-if="currentSection === index.toString()"
+        :src="`car${index + 1}.jpg`"
+        alt=""
+      />
+    </div>
+  </div>
+</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { ref, reactive, onMounted } from 'vue';
+
+	const headings = reactive(['King', 'Lightning Mcqueen', 'Sally']);
+
+	const currentSection = ref();
+
+	onMounted(() => {
+		const headerElements = document.querySelectorAll('.header');
+
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					currentSection.value = entry.target.id.toString();
+				}
+			});
+		});
+
+		headerElements.forEach(header => {
+			observer.observe(header);
+		});
+	});
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+
+/* Picture Container */
+.fixed-picture-container {
+  position: sticky;
+  top: 0;
+  right: 0;
+  overflow-y: auto;  /* Adjust the background color as needed */
+  padding: 1rem;
+  box-sizing: border-box;
+}
+
+</style>
